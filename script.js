9d4c51194822e68c428f571ed5c50b0c9ae92c9c
Heidinger Sánchez <heidingeraldair2016@gmail.com>
2024-02-05 17:47:33 -0500

    Login

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnSignUp = document.getElementById('sign-up'),
+    btnSignIn = document.getElementById('sign-in'), 
+    formregister = document.querySelector('.register'),
+    formlogin = document.querySelector('.login');
+
+btnSignIn.addEventListener('click', e => {
+    formregister.classList.add('hide');
+    formlogin.classList.remove('hide');
+})
